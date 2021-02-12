1e9b4302c579da4234036ee398702f1317a3fd9a
Mozahidul01 <mozo.postbox@gmail.com>
2021-02-13 01:45:00 +0600

    Task Add button not working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
 import {useState} from 'react'
-import Header from './components/Header/Header';
-import Tasks from './components/Tasks/Tasks';
-import AddTask from './components/Tasks/AddTask';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState( [
     {
-       id: '1',
+       id: 1,
        text: 'Doctors Appointment',
        day: 'Mar 5th at 2:30pm',
        reminder: true,
     },
     {
-       id: '2',
+       id: 2,
        text: 'Meeting at School',
        day: 'Mar 7th at 1:30pm',
        reminder: true,
     },
     {
-       id: '3',
+       id: 3,
        text: 'Food Shopping',
        day: 'Mar 5th at 4:30pm',
        reminder: true,
@@ -38,6 +39,7 @@ function App() {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task}
     setTasks({ ...tasks, newTask });
+    console.log(newTask , task)
   }
 
   //Toggle Reminder
@@ -49,9 +51,19 @@ function App() {
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask onAdd={addTask}/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : <h3 style = {{textAlign: 'center',paddingTop: '8px'}}>No Task Available</h3>}
+      <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+      
+      {showAddTask && <AddTask onAdd={addTask} />}
+      
+      {tasks.length > 0 ? 
+      <Tasks 
+      tasks={tasks} 
+      onDelete={deleteTask} 
+      onToggle={toggleReminder}/> : 'No Task Available'}
+      
     </div>
   );
 }
